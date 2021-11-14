9acc9b04341daf882756f8d5f11f734e66c7a658
BaldiriDeLaCosta <joel.romero@enti.cat>
2021-11-14 12:06:30 +0100

    a

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var config=
     type: Phaser.AUTO,
     width:800,
     height:600,
-    scene:[gameState], //array con los niveles
+    scene:[gameState],
     render:{
         pixelArt:true
     },
